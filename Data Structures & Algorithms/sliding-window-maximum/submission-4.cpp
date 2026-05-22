6d2b273b28e66a44be2d7afe53992639b522705b
Akshat3112000 <134837163+Akshat3112000@users.noreply.github.com>
2026-05-22 17:03:45 +0530

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& a, int k) {
+        // Boost I/O speed
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        int n = a.size();
+        if (n == 0) return {};
+
+        deque<int> dq;
+        // Pre-allocate the exact capacity to avoid reallocations
+        vector<int> ans;
+        ans.reserve(n - k + 1); 
+
+        for (int i = 0; i < k; i++) {
+            while(!dq.empty() && a[dq.back()] <= a[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        ans.push_back(a[dq.front()]);
+
+        for(int i = k; i < n; i++) {
+            while(!dq.empty() && a[dq.back()] <= a[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(dq.front() == i - k) dq.pop_front();
+            ans.push_back(a[dq.front()]);
+        }
+        return ans;
+    }
+};
