3a9449b46c0073c1a4e32d42d6ef741261240638
Akshat3112000 <134837163+Akshat3112000@users.noreply.github.com>
2026-06-16 14:20:45 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.cpp
@@ -0,0 +1,88 @@
+class LRUCache {
+private:
+    struct Node {
+        int key;
+        int val;
+        Node* prev;
+        Node* next;
+        Node(int k, int v) : key(k) , val(v) , prev(nullptr) , next(nullptr){}
+    };
+
+    int capacity;
+    unordered_map<int,Node*> cache;
+
+    Node* head;
+    Node* tail;
+
+    void add(Node* x)
+    {
+        head -> next -> prev = x;
+        x -> next = head -> next;
+        head -> next = x;
+        x -> prev = head;
+    }
+
+    void remove(Node* x)
+    {
+        x -> prev -> next = x -> next;
+        x -> next -> prev = x -> prev;
+    }
+
+    void move(Node* x)
+    {
+        remove(x);
+        add(x);
+    }
+
+public:
+    
+    LRUCache(int cap) {
+        capacity = cap;
+
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
+
+        head -> next = tail;
+        tail -> prev = head;
+    }
+    
+    int get(int k) {
+        if(cache.find(k) == cache.end())
+        {
+            return -1;
+        }
+
+        move(cache[k]);
+        return cache[k] -> val;
+    }
+    
+    void put(int k, int v) {
+        if(cache.find(k) != cache.end())
+        {
+            move(cache[k]);
+            cache[k] -> val = v;
+            return;
+        }
+
+        if(cache.size() == capacity)
+        {
+            Node* node = tail -> prev;
+            cache.erase(node -> key);
+            remove(node);
+            delete node;
+        }
+
+        Node* node = new Node(k,v);
+        cache[k] = node;
+        add(node);
+    }
+
+    ~LRUCache() {
+    Node* current = head;
+    while (current) {
+        Node* nextNode = current->next;
+        delete current; // Cleanly destroys every remaining node
+        current = nextNode;
+    }
+}
+};
