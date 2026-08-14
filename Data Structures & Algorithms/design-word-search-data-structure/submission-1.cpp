ae57bd52a65964b4c787411add7adbc4be963e6b
Akshat3112000 <134837163+Akshat3112000@users.noreply.github.com>
2026-08-14 15:54:25 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.cpp
@@ -0,0 +1,60 @@
+class WordDictionary {
+    struct TrieNode {
+        TrieNode* a[26];
+        bool isWord;
+        TrieNode() {
+            isWord = 0;
+            for (int i = 0; i < 26; i++) {
+                a[i] = nullptr;
+            }
+        }
+    };
+    TrieNode* trie;
+
+   public:
+    WordDictionary() { trie = new TrieNode(); }
+
+    void addWord(string word) {
+        int n = word.length();
+        TrieNode* cur = trie;
+        int i = 0;
+
+        while (i < n && cur->a[word[i] - 'a']) {
+            cur = cur->a[word[i] - 'a'];
+            i++;
+        }
+        for (; i < n; i++) {
+            cur->a[word[i] - 'a'] = new TrieNode();
+            cur = cur->a[word[i] - 'a'];
+        }
+
+        cur->isWord = 1;
+    }
+
+    bool search(string word) {
+        queue<TrieNode*> q;
+        q.push(trie);
+        int n;
+        for (char c : word) {
+            n = q.size();
+            if (n == 0) break;
+            while (n--) {
+                TrieNode* node = q.front();
+                q.pop();
+                if (c != '.') {
+                    if (node->a[c - 'a']) q.push(node->a[c - 'a']);
+                    continue;
+                }
+                for (int i = 0; i < 26; i++) {
+                    if (node->a[i]) q.push(node->a[i]);
+                }
+            }
+        }
+        while (!q.empty()) {
+            TrieNode* node = q.front();
+            q.pop();
+            if (node->isWord) return 1;
+        }
+        return 0;
+    }
+};
